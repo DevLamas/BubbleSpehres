5396073a5fcc9fcdc4c241e6f59077378f58e462
justinbah <bahier.justin@gmail.com>
2021-01-08 17:34:13 +0100

    Ajout finish

---
+++ Assets/Scripts/FinishBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishBehaviour : MonoBehaviour
+{
+    public GameBehaviour GameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager = GameObject.Find("GameManager").GetComponent<GameBehaviour>();
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Récupération de la potion par le joueur
+        if (collision.gameObject.name == "Player")
+        {
+            // Destruction de l'objet parent
+            GameManager.Finish() ;
+        }
+    }
+}
---
+++ Assets/Scripts/GameBehaviour.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameBehaviour : MonoBehaviour
+{
+
+    private int ViePlayer = 2;
+    public bool showWinScreen = false;
+    public bool showLooseSreen = false;
+    public string labelResultat = "";
+
+    public void Finish()
+    {
+
+        showWinScreen = true;
+        Time.timeScale = 0f;
+
+        Debug.Log("test");
+
+    }
+    public int HP
+    {
+        get { return ViePlayer; }
+        set
+        {
+            ViePlayer = value;
+
+            if (ViePlayer <= 0)
+            {
+                labelResultat = "☠GAME OVER☠";
+                showLooseSreen = true;
+                Time.timeScale = 0;
+
+            }
+
+            Debug.LogFormat("vie : {0}", ViePlayer);
+
+        }
+
+    }
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(20, 20, 150, 25), "Santé du joueurs:" + ViePlayer);
+        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300, 50), labelResultat);
+
+        if (showWinScreen)
+        {
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "Vous avez gagné !"))
+            {
+
+                SceneManager.LoadScene(0);
+                Time.timeScale = 1.0f;
+
+            }
+        }
+        if (showLooseSreen)
+        {
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "Vous avez perdu !"))
+            {
+
+                SceneManager.LoadScene(0);
+                Time.timeScale = 1.0f;
+
+
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
