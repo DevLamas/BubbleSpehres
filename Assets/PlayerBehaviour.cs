e6945d4cd2dead9097c4d7dbfa4baf0af003428c
justinbah <bahier.justin@gmail.com>
2021-01-08 11:41:24 +0100

    Ajout deplacement

---
+++ Assets/PlayerBehaviour.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+
+
+
+    public float VitesseMouvement = 10f;
+    public float VitesseRotation = 100f;
+    public float SautVelocite = 20f;
+    
+    
+    public float DistanceGround = 0.1f;
+    public LayerMask groundLayer;
+    private Rigidbody _rb;
+    private CapsuleCollider _col;
+
+    public float vInput;
+    public float hInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _col = GetComponent<CapsuleCollider>();
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        vInput = Input.GetAxis("Vertical") * VitesseMouvement;
+        hInput = Input.GetAxis("Horizontal") * VitesseRotation;
+
+
+    }
+    private void FixedUpdate()
+    {
+
+        Vector3 Rotation = Vector3.up * hInput;
+        Quaternion angleRot = Quaternion.Euler(Rotation * Time.fixedDeltaTime);
+        _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
+        _rb.MoveRotation(_rb.rotation * angleRot);
+
+
+
+
+    }
+
+
+}
